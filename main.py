530b15f6eddc1fb1fb9ca1ad7aa2397aa8188d73
Manikumar1998 <manikumarperla18@gmail.com>
2018-09-27 02:00:20 +0530

    Add get_limits() function: Returns list of min and max of all features Add construct_decision_tree: business logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import random
+import numpy as np
 
 class Node(object):
     def __init__(self, feature, value, left, right, _class=None):
@@ -10,6 +11,35 @@ class Node(object):
         self.right = right
         self._class = _class
 
+def cal_entropy(a, b, c, d):
+    #TO-DO: calculate the entropy
+    return None
+        
+def construct_decision_tree(dataset, limits, classes, m, n):
+    X = dataset['X']
+    Y = dataset['Y']
+
+    for f_index in xrange(n):
+        _min = limits[f_index]['min']
+        _max = limits[f_index]['max']
+        for value in xrange(np.linspace(_min, _max, 100)):
+            entropy = cal_entropy(dataset, classes, f_index, value)
+
+def get_limits(X, n):
+    limits = []
+    for _ in xrang(n):
+        limits.append({'min':None, 'max':None})
+
+    for vector in X:
+        for index, value in enumerate(vector):
+            if not limits[index]['min'] or (value < limits[index]['min']):
+                limits[index]['min'] = value
+                
+            if not limits[index]['max'] or (value > limits[index]['max']):
+                limits[index]['max'] = value
+
+    return limits
+    
 def extract_data(data, ratio):
     split_len = len(data)*ratio/100
     training_set = []
@@ -32,15 +62,19 @@ def extract_data(data, ratio):
         Y_test.append(int(vector.pop(-1)))
         X_test.append(map(float, vector))
 
-    return X_train, Y_train, X_test, Y_test
+    classes = list(set(Y_train+Y_test))
+    return X_train, Y_train, X_test, Y_test, classes
 
 if __name__ == "__main__":
     with open("banknote_auth_data.txt", 'r') as fp:
         data = fp.readlines()
     m = len(data)
-    X_train, Y_train, X_test, Y_test = extract_data(data, 90)
+    X_train, Y_train, X_test, Y_test, classes = extract_data(data, 90)
     n = len(X_train[0])
+    dataset {'X':X_train, 'Y':Y_train}
 
-    
+    #Should we get min of only X_train or X_train+X_test?
+    limits = get_limits(X_train, n)
+    construct_decision_tree(dataset, limits, classes, m, n)
 
     
