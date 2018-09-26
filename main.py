efe75f97233a460a21b1fe2f53df197f7e9fda75
Manikumar1998 <manikumarperla18@gmail.com>
2018-09-27 00:53:37 +0530

    Added split_data which returns Training and Test data

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import sys
+import time
+import random
+
+def split_data(data, ratio):
+    split_len = len(data)*ratio/100
+    training_set = []
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+    
+    while(len(training_set) < split_len):
+        index = random.randint(0, len(data)-1)
+        training_set.append(data.pop(index))
+
+    for vector in training_set:
+        vector = map(str.strip, vector.split(','))
+        Y_train.append(int(vector.pop(-1)))
+        X_train.append(map(float, vector))
+
+    for vector in data:
+        vector = map(str.strip, vector.split(','))
+        Y_test.append(int(vector.pop(-1)))
+        X_test.append(map(float, vector))
+
+    return X_train, Y_train, X_test, Y_test
 
 if __name__ == "__main__":
     fp = open("banknote_auth_data.txt", 'r')
     data = fp.readlines()
     m = len(data)
-    Y = []
-    X = []
-    print len(data)
-    for vector in data:
-        print map(str.strip, vector.split(',')).pop(-1)
-        # X.append(map(float, vector.split(',')))
-        # Y.append(X.pop(-1))
-    print X[0], Y[0]
+    X_train, Y_train, X_test, Y_test = split_data(data, 90)
+    n = len(X_train[0])
     fp.close()
